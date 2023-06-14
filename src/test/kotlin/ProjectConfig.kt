c1a2e8d14f0bbec4aa115287a58864e996b39691
namkyu.jang <nkjang@gmail.com>
2023-06-15 08:32:12 +0900

    Initial commit

---
+++ src/test/kotlin/ProjectConfig.kt
@@ -0,0 +1,8 @@
+package my.ex
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.extensions.spring.SpringExtension
+
+class ProjectConfig : AbstractProjectConfig() {
+    override fun extensions() = listOf(SpringExtension)
+}
---
+++ src/test/kotlin/my/ex/controller/MyControllerTest.kt
@@ -0,0 +1,8 @@
+package my.ex.controller 
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class MyControllerTest : FunSpec({
+
+  
+})
